14ba6db130c3d5209682029b56c5d2d40411fde7
Rawand Dargali <rdargali@yahoo.com>
2019-10-01 09:42:41 -0500

    second commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var emergencyButton = document.getElementById('map-event');
+
+
+emergencyButton.addEventListener('click', function(){
+    getLocation();
+});
+
+
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
