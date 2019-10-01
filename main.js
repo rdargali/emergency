bc2269a379cb6a9ffa8f4c7beec978be88abda67
dgr8ninja <dr4602@gmail.com>
2019-10-01 13:08:52 -0500

    First commit for ponderupdates

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
-var emergencyButton = document.getElementById('map-event');
+// var emergencyButton = document.getElementById('map-event');
+// console.log(emergencyButton)
+// emergencyButton.addEventListener('click', function(){
+//    console.log("nothing")
+// });
+console.log("map has loaded on new page")
+var map, infoWindow;
 
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 13
+        });
+        infoWindow = new google.maps.InfoWindow;
 
-emergencyButton.addEventListener('click', function(){
-    getLocation();
-});
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
 
-var x = document.getElementById("demo");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
-}
-
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+      google.maps.event.addDomListener(window, 'load', initMap)
